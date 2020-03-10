0fbc25aa5b4935c94b3794dc72f2bcc089703077
Robert Adrian Bucur <adrian_bucur49@ymail.com>
2020-03-10 15:45:30 +0200

    Added project files

---
+++ TicTacToe/Xsi0/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Xsi0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Location = new System.Drawing.Point(17, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.BackColor = System.Drawing.Color.LightGray;
+            this.button2.Location = new System.Drawing.Point(133, 17);
+            this.button2.Margin = new System.Windows.Forms.Padding(8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.BackColor = System.Drawing.Color.LightGray;
+            this.button3.Location = new System.Drawing.Point(249, 17);
+            this.button3.Margin = new System.Windows.Forms.Padding(8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button4.BackColor = System.Drawing.Color.LightGray;
+            this.button4.Location = new System.Drawing.Point(17, 133);
+            this.button4.Margin = new System.Windows.Forms.Padding(8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button5.BackColor = System.Drawing.Color.LightGray;
+            this.button5.Location = new System.Drawing.Point(133, 133);
+            this.button5.Margin = new System.Windows.Forms.Padding(8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "-";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button6.BackColor = System.Drawing.Color.LightGray;
+            this.button6.Location = new System.Drawing.Point(249, 133);
+            this.button6.Margin = new System.Windows.Forms.Padding(8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "-";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Click);
+            // 
+            // button7
+            // 
+            this.button7.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button7.BackColor = System.Drawing.Color.LightGray;
+            this.button7.Location = new System.Drawing.Point(17, 249);
+            this.button7.Margin = new System.Windows.Forms.Padding(8);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "-";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.Click);
+            // 
+            // button8
+            // 
+            this.button8.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button8.BackColor = System.Drawing.Color.LightGray;
+            this.button8.Location = new System.Drawing.Point(133, 249);
+            this.button8.Margin = new System.Windows.Forms.Padding(8);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "-";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Click);
+            // 
+            // button9
+            // 
+            this.button9.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button9.BackColor = System.Drawing.Color.LightGray;
+            this.button9.Location = new System.Drawing.Point(249, 249);
+            this.button9.Margin = new System.Windows.Forms.Padding(8);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 100);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "-";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(371, 366);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.Name = "Form1";
+            this.Text = "X si 0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ TicTacToe/Xsi0/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Xsi0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool ok = true;
+        private int nr = 0;
+
+        private void Click(object sender, EventArgs e)
+        {
+            Button B = new Button();
+            B = sender as Button;
+            if (ok == true && B.Text == "-")
+            {
+                B.Text = "X";
+                ok = false;
+            }
+            else
+                if (B.Text == "-")
+                {
+                    B.Text = "0";
+                    ok = true;
+                }
+            nr++;
+            if (button1.Text != "-")
+                if (button1.Text == button5.Text && button1.Text == button9.Text)
+                {
+                    nr = 0;
+                    button1.BackColor = button5.BackColor = button9.BackColor = Color.Red;
+                    MessageBox.Show(button1.Text + " a castigat!");
+                    button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "-";
+                    button1.BackColor = button5.BackColor = button9.BackColor = Color.LightGray;
+                }
+            if (button1.Text != "-")
+                if (button1.Text == button2.Text && button1.Text == button3.Text)
+                {
+                    nr = 0;
+                    button1.BackColor = button2.BackColor = button3.BackColor = Color.Red;
+                    MessageBox.Show(button1.Text + " a castigat!");
+                    button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "-";
+                    button1.BackColor = button2.BackColor = button3.BackColor = Color.LightGray;
+                }
+            if (button1.Text != "-")
+                if (button1.Text == button4.Text && button1.Text == button7.Text)
+                {
+                    nr = 0;
+                    button1.BackColor = button4.BackColor = button7.BackColor = Color.Red;
+                    MessageBox.Show(button1.Text + " a castigat!");
+                    button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "-";
+                    button1.BackColor = button4.BackColor = button7.BackColor = Color.LightGray;
+                }
+            if (button2.Text != "-")
+                if (button2.Text == button5.Text && button2.Text == button8.Text)
+                {
+                    nr = 0;
+                    button2.BackColor = button5.BackColor = button8.BackColor = Color.Red;
+                    MessageBox.Show(button2.Text + " a castigat!");
+                    button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "-";
+                    button2.BackColor = button5.BackColor = button8.BackColor = Color.LightGray;
+                }
+            if (button3.Text != "-")
+                if (button3.Text == button5.Text && button3.Text == button7.Text)
+                {
+                    nr = 0;
+                    button3.BackColor = button5.BackColor = button7.BackColor = Color.Red;
+                    MessageBox.Show(button3.Text + " a castigat!");
+                    button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "-";
+                    button3.BackColor = button5.BackColor = button7.BackColor = Color.LightGray;
+                }
+            if (button3.Text != "-")
+                if (button3.Text == button6.Text && button3.Text == button9.Text)
+                {
+                    nr = 0;
+                    button3.BackColor = button6.BackColor = button9.BackColor = Color.Red;
+                    MessageBox.Show(button3.Text + " a castigat!");
+                    button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "-";
+                    button3.BackColor = button6.BackColor = button9.BackColor = Color.LightGray;
+                }
+            if (button4.Text != "-")
+                if (button4.Text == button5.Text && button4.Text == button6.Text)
+                {
+                    nr = 0;
+                    button4.BackColor = button5.BackColor = button6.BackColor = Color.Red;
+                    MessageBox.Show(button4.Text + " a castigat!");
+                    button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "-";
+                    button4.BackColor = button5.BackColor = button6.BackColor = Color.LightGray;
+                }
+            if (button7.Text != "-")
+                if (button7.Text == button8.Text && button7.Text == button9.Text)
+                {
+                    nr = 0;
+                    button7.BackColor = button8.BackColor = button9.BackColor = Color.Red;
+                    MessageBox.Show(button7.Text + " a castigat!");
+                    button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "-";
+                    button7.BackColor = button8.BackColor = button9.BackColor = Color.LightGray;
+                }
+            if (nr == 9)
+            {
+                MessageBox.Show("Remiza!");
+                button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "-";
+                nr = 0;
+            }
+        }
+
+    }
+}
